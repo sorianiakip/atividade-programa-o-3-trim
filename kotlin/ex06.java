c1ce7626776aecfa6e323009dcfb0d808491148a
Ana Soriani <ana.soriani.nunes@escola.pr.gov.br>
2026-09-24 14:39:38 -0300

    Add Exercicio06 class for personal introduction

---
+++ kotlin/ex06.java
@@ -0,0 +1,12 @@
+public class Exercicio06 {
+    public static void main(String[] args) {
+        String nome = "Ana";
+        int idade = 17;
+        String turma = "3DS";
+
+        System.out.println("APRESENTAÇÃO PESSOAL");
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Turma: " + turma);
+    }
+}
